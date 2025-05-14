122226de2a08f6832086c05cb4f0cb70671b719c
Ifan Maull <ifanmaul03@gmail.com>
2025-05-14 12:07:34 +0700

    Responsi PST Paket A

--- H1D022012_MuhammadIfanSidiqM_Responsi-PST_ShiftD_PaketA/src/main.cpp
+++ H1D022012_MuhammadIfanSidiqM_Responsi-PST_ShiftD_PaketA/src/main.cpp
@@ -14,29 +14,28 @@ void setup() {
   pinMode(ledWarning, OUTPUT);
   pinMode(ledSafe, OUTPUT);
 
-  Serial.begin(9600); // Ganti dari 1000 ke 9600 agar standar dan lebih stabil
+  Serial.begin(9600);
 }
 
 void loop() {
-  // Kirim sinyal trigger ke sensor
+
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
 
-  // Baca durasi pantulan dari echo
   duration = pulseIn(echoPin, HIGH);
 
-  // Hitung jarak dalam cm
+  
   distance = duration * 0.034 / 2;
 
-  // Tampilkan di serial monitor
+  
   Serial.print("Distance: ");
   Serial.print(distance);
   Serial.println(" cm");
 
-  // Nyalakan LED sesuai kondisi
+  
   if (distance <= 10) {
     digitalWrite(ledWarning, HIGH);
     digitalWrite(ledSafe, LOW);     
